b0bdaab3fe4674bdb0cf84c2df2f5fc40905bfb0
Matas Seputis <matasseputis@gmail.com>
2022-01-04 21:29:18 +0100

    User content with JWT Claims

--- Tweetbook/Data/DataContext.cs
+++ Tweetbook/Data/DataContext.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using Tweetbook.Domain;
 
